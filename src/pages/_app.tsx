ba464155c421416796e590e551ce970b633921cf
Filipe Marques <filipen@rpc.com.br>
2020-09-01 19:47:19 -0300

    add pwa support

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,7 @@ const App = ({ Component, pageProps }: AppProps) => {
       <Head>
         <title>VUTTR</title>
         <link rel="shortcut icon" href="#" />
+        <link rel="manifest" href="/manifest.json" />
         <meta name="description" content="Very Useful Tools to Remember" />
       </Head>
       <GlobalStyle />
