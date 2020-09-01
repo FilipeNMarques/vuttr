724b8a1f82489b162a400ff6d558017707ce1ecb
Filipe Marques <filipen@rpc.com.br>
2020-09-01 19:32:07 -0300

    feature: Add Main component

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,12 @@
+import { Wrapper, Title, Description } from './styles'
+
+const Main = () => {
+  return (
+    <Wrapper>
+      <Title>Hello Main</Title>
+      <Description>This is a description</Description>
+    </Wrapper>
+  )
+}
+
+export default Main
